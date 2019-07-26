75d489668d5a95d3badb14d50af4b1630918ea27
harman50 <52832290+harman50@users.noreply.github.com>
2019-07-26 19:59:12 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import {BookInformation} from '../../app/bookInfo';
+
+export const MYBOOKS : BookInformation[] = 
+[
+    { bname:'King Of Malorn' , aname: 'Annie Douglass Lima' , genre:'Art' , year:2000 , picture:'assets/images/KingOfMalornfinal.jpg'},
+    { bname: 'The Midnight front' , aname: 'David Mack', genre:'Art' , year:1995 , picture:'assets/images/Midnight.jpg'},
+    { bname: 'Folk and farytale' , aname : 'Martin Hallett', genre:'Stories', year:1991 , picture:'assets/images/Folk.jpg'},
+    { bname: 'Pride and Prejudice', aname: 'Jane Austen', genre:'romantic' ,year:2000 ,picture:'assets/images/pride.jpg'}
+
+]
